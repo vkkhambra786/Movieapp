f8b25a0f4b4f65bf7629dfe47329b20209a7e3b6
vkkhambra786 <vkkhambra786@gmail.com>
2022-03-16 17:40:06 +0530

    HII

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -5,10 +5,10 @@ const Movie = (props) => {
         src="http://dummyimage.com/250x250.png/5fa2dd/ffffff"
         alt="Movie-Thumbnail"
       />
-      <h3>Name- {props.movie_name}</h3>
-      <div>Type- {props.genre}</div>
-      <h4>Rating- {props.rating}</h4>
-      <div>Release- {props.release_date}</div>
+      <h3>Name :{props.movie_name}</h3>
+      <div>Type :{props.genre}</div>
+      <h4>Rate :{props.rating}</h4>
+      <div>RDate : {props.release_date}</div>
     </div>
   );
 };
